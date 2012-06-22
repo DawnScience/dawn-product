394fe613a11d0e20dfd9498cb8e68406fe6873b0
matthew.gerring <matthew.gerring@gmail.com>
2012-06-22 15:01:52 +0100

    Fixed dawb in batch files.
    Conflicts:
    	org.dawnsci.base.product.feature/DAWN.launch

--- org.dawb.workbench.config.feature/win32/batch.bat
+++ org.dawb.workbench.config.feature/win32/batch.bat
@@ -4,7 +4,7 @@ REM ##################################################################
 REM # Please enter the paths required to run the workflow here
 
 REM # Where your workspace is located (under the File-Switch Workspace menu in the UI)
-set WORKSPACE=C:/users/<your name>/dawb_workspace
+set WORKSPACE=C:/users/<your name>/dawn_workspace
 
 REM # The full path to where the model is located
 set MODEL=%WORKSPACE%/workflows/examples/maths_example.moml
@@ -15,4 +15,4 @@ HOSTTYPE=x86
 ##################################################################
 # Nothing to do here...
 
-./dawb -noSplash -application com.isencia.passerelle.workbench.model.launch -data %WORKSPACE% -consolelog -os linux -ws gtk -arch %HOSTTYPE% -vmargs -Dmodel=%MODEL%
+./dawn -noSplash -application com.isencia.passerelle.workbench.model.launch -data %WORKSPACE% -consolelog -os linux -ws gtk -arch %HOSTTYPE% -vmargs -Dmodel=%MODEL%
