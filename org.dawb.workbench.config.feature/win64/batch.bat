13f3d9cdeaa8939af11e58081e3354d13fcd9813
matthew.gerring <matthew.gerring@gmail.com>
2012-06-25 16:00:51 +0100

    Fixes found after doing batch cheat sheet on windows.

--- org.dawb.workbench.config.feature/win64/batch.bat
+++ org.dawb.workbench.config.feature/win64/batch.bat
@@ -4,15 +4,13 @@ REM ##################################################################
 REM # Please enter the paths required to run the workflow here
 
 REM # Where your workspace is located (under the File-Switch Workspace menu in the UI)
-set WORKSPACE=C:/users/<your name>/dawn_workspace
+set WORKSPACE=C:/Users/<your user name>/workspace
 
 REM # The full path to where the model is located
 set MODEL=%WORKSPACE%/workflows/examples/maths_example.moml
 
-REM # This script assumes that 'HOSTTYPE' is set, change or set it here if not
-HOSTTYPE=x86_64
 
 ##################################################################
 # Nothing to do here...
 
-./dawn -noSplash -application com.isencia.passerelle.workbench.model.launch -data %WORKSPACE% -consolelog -os linux -ws gtk -arch %HOSTTYPE% -vmargs -Dmodel=%MODEL%
+dawn -noSplash -application com.isencia.passerelle.workbench.model.launch -data %WORKSPACE% -consolelog -vmargs -Dmodel=%MODEL%
