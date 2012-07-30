22fa09e30287d6f9bc8ccbaeb7bf218dcbb12dfe
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2012-07-30 13:49:14 +0100

    DAWN intro: cleaning of of the HTML files (script tags would break the welcome page on some OS like ubuntu)

--- org.dawnsci.intro/content/js/syndication.js
+++
@@ -1,8 +0,0 @@
-var newcontent = document.createElement('contentProvider');
-var newId = 'AAAAAAAAAAAAA';
-newcontent.id = newId;
-newcontent.setAttribute('pluginId', 'org.dawnsci.intro');
-newcontent.setAttribute('class', 'org.dawnsci.intro.content.ASinglePerspectiveContentProvider');
-newcontent.appendChild(document.createTextNode('Here is some syndicated content.'));  
-var scr = document.getElementById('syndication');
-scr.parentNode.insertBefore(newcontent, scr); 
