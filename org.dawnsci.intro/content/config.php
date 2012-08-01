f2655162980ad15d53bef589b860ce299f955da7
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2012-08-01 10:11:21 +0100

    Dawn intro: *cleaning up *tutorial/cheatsheets intro page

--- org.dawnsci.intro/content/config.php
+++
@@ -1,3 +0,0 @@
-<?php
-    $email_to =   'baha.el-kassaby@diamond.ac.uk'; //the address to which the email will be sent
-?>
---
+++ org.dawnsci.intro/src/org/dawnsci/intro/actions/OpenCheatsheetAction.java
@@ -0,0 +1,48 @@
+/*-
+ * Copyright (c) 2012 European Synchrotron Radiation Facility,
+ *                    Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.dawnsci.intro.actions;
+
+import java.util.Properties;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.cheatsheets.OpenCheatSheetAction;
+import org.eclipse.ui.intro.IIntroSite;
+import org.eclipse.ui.intro.config.IIntroAction;
+
+/**
+ * Class to open a cheatsheet through an org.eclipse.ui.intro/runAction
+ * 
+ * @author Baha El Kassaby
+ * 
+ */
+public class OpenCheatsheetAction implements IIntroAction {
+
+	private static final String ATT_ID = "id"; //$NON-NLS-1$
+
+	@Override
+	public void run(IIntroSite site, Properties params) {
+		String id = params.getProperty(ATT_ID);
+		final Action action = new OpenCheatSheetAction(id);
+		
+		//close the intro part
+//		IIntroPart part = PlatformUI.getWorkbench().getIntroManager().getIntro();
+//		PlatformUI.getWorkbench().getIntroManager().closeIntro(part);
+		//run the appropriate launcher
+		Display.getDefault().asyncExec(new Runnable(){
+			@Override
+			public void run() {
+				if (action != null)
+					action.run();
+			}
+			
+		});
+	}
+}
