9339c49e3ce7902f04b138e5f18cfc216cf2cc79
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2012-08-01 13:51:00 +0100

    DAWN Intro: feedback action to open the view

---
+++ org.dawnsci.intro/src/org/dawnsci/intro/actions/OpenFeedbackViewAction.java
@@ -0,0 +1,40 @@
+/*-
+ * Copyright (c) 2012 European Synchrotron Radiation Facility,
+ *                    Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.dawnsci.intro.actions;
+
+import java.util.Properties;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.intro.IIntroSite;
+import org.eclipse.ui.intro.config.IIntroAction;
+
+public class OpenFeedbackViewAction implements IIntroAction {
+
+	public static final String ID = "uk.ac.diamond.scisoft.views.FeedbackView";
+
+	@Override
+	public void run(IIntroSite site, Properties params) {
+		Display.getDefault().asyncExec(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
+							.showView(ID);
+				} catch (PartInitException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
