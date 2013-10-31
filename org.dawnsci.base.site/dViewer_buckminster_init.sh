1718a0390a4863d80aa6e2bae08307c04e9ddc31
Gábor Náray <gnaray@embl.fr>
2013-10-31 19:14:56 +0100

    Improved error checking, python version checker, java version forcer.

--- org.dawnsci.base.site/dViewer_buckminster_init.sh
+++ org.dawnsci.base.site/dViewer_buckminster_init.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 #Input: output folder
-#Output: CURRENT_PATH, PATH, BUILDER_WITH_COMMON_ARGS, PRODUCT_ID, PRODUCT_FOLDER, and installed buckminster
+#Output: CURRENT_PATH, PATH, BUILDER_WITH_COMMON_ARGS, PRODUCT_ID, PRODUCT_WORKSPACE_FOLDER, and installed buckminster
 
-[ "${BASH_SOURCE[0]}" != "${0}" ] && THIS_SOURCED=1 || THIS_SOURCED=0
-if [ -z "${1}" ] || [ ! -d "${1}" ]; then
-  echo "Usage: ${0} <output folder>"
-  echo "output folder: it must exist, and it must not be in git repository (it would interfere)."
-  echo "The products folder in the output folder will contain the built products."
-  [[ "${THIS_SOURCED}" -ne 0 ]] && return 1 || exit 1
-fi
 export PRODUCT_ID="dViewer"
 BUCKMINSTER_URL="http://download.eclipse.org/tools/buckminster/headless-4.2/"
 DIRECTOR_FILE="director_latest.zip"
@@ -19,16 +12,76 @@ BUCKMINSTER_FOLDER="buckminster"
 DIRECTOR_FOLDER="director"
 BUILDER_FILE="dawn.py" #This could be "${PRODUCT_ID}.py" if it would be customised, but currently there is no need
 UPDATE_BUILDER="" #Set this to anything to update the BUILDER_FILE
+PYTHON_VERSION_REQUIRED="2.6"
+USE_JAVA_VERSION="v1.7.0-45"
+export PRODUCT_WORKSPACE_FOLDER="dawnbase" #This could be "${PRODUCT_ID}_base" if the buckminster files would be customised as well
 
-export PRODUCT_FOLDER="dawnbase" #This could be "${PRODUCT_ID}_base" if the buckminster files would be customised as well
-#Resolving output folder, because symlinks confuse the director and buckminster
-WORKSPACE_PATH=`readlink -e "${1}"`
 export CURRENT_PATH=`pwd`
+
+[ "${BASH_SOURCE[0]}" != "${0}" ] && THIS_SOURCED=1 || THIS_SOURCED=0
+[ -z "$PXSOFT" ] && pxsoft="/opt/pxsoft" || pxsoft="$PXSOFT"
+pxsoft_basedir="${PRODUCT_ID}"
+
+is_equallessthan() {
+  [ "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
+}
+
+is_lessthan() {
+  [ "$1" = "$2" ] && return 1 || is_equallessthan $1 $2
+}
 
 error=0
 while true; do
+  if [ -z "${1}" ] || [ ! -d "${1}" ]; then
+    error=1
+    echo "Usage: ${0} <output folder>"
+    echo "output folder: it must exist, and it must not be in git repository (it would interfere)."
+    echo "The products folder in the output folder will contain the built products."
+    break
+  fi
+  #Checking if is_lessthan works
+  if ! is_lessthan "1" "2"; then
+    error=1
+    echo "Error (${error}): can not compare versions (probably too old sort)"
+    break
+  fi
+  . ${pxsoft}/bin/.pxsoft_wrapper_functions ${pxsoft}/bin/${pxsoft_basedir}
+  error=$?
+  if [ ${error} -ne 0 ]; then
+    echo "Error (${error}): can not initialize pxsoft functions"
+    break
+  fi
+  #Checking python version
+  python_version=`python -V 2>&1 | grep -oe "Python [[:digit:]\.]*" | grep -oe "[[:digit:]\.]*"`
+  error=$?
+  if [ ${error} -ne 0 ]; then
+    echo "Error (${error}): can not determine version of python (probably too old grep)"
+    break
+  fi
+  if is_lessthan "${python_version}" "${PYTHON_VERSION_REQUIRED}"; then
+    error=1
+    echo "Error (${error}): python version ${PYTHON_VERSION_REQUIRED} is required"
+    break
+  fi
+  #Resolving output folder, because symlinks confuse the director and buckminster
+  WORKSPACE_PATH=`readlink -e "${1}" 2>/dev/null`
+  error=$?
+  if [ ${error} -ne 0 ]; then
+    WORKSPACE_PATH=`readlink -f "${1}"`
+    error=$?
+    if [ ${error} -ne 0 ]; then
+      echo "Error (${error}): can not resolve ${1} (maybe too old readlink)"
+      break
+    fi
+  fi
+  #Changing directory to workspace
+  cd "${WORKSPACE_PATH}"
+  error=$?
+  if [ ${error} -ne 0 ]; then
+    echo "Error (${error}): can not change to ${WORKSPACE_PATH} folder"
+    break
+  fi
   #Checking if git command is available and if yes, the output folder is not in git repository
-  echo "Checking if output folder ${WORKSPACE_PATH} is not in git repository."
   git --version >/dev/null
   error=$?
   if [ ${error} -ne 0 ]; then
@@ -43,11 +96,11 @@ while true; do
     break
   fi
   error=0
-  #Changing directory to workspace
-  cd "${WORKSPACE_PATH}"
+  #Switching to excepted java version
+  use_pxsoft_package use_java --PXSOFT_VERSION ${USE_JAVA_VERSION} -version
   error=$?
   if [ ${error} -ne 0 ]; then
-    echo "Error (${error}): can not change to ${WORKSPACE_PATH} folder"
+    echo "Error (${error}): can not switch to ${USE_JAVA_VERSION} in pxsoft"
     break
   fi
   if [ ! -r "${DIRECTOR_FILE}" ]; then
@@ -126,8 +179,13 @@ while true; do
   if [ ! -r "${WORKSPACE_PATH}/.keyring" ]; then
     echo "Creating .keyring file."
     touch ${WORKSPACE_PATH}/.keyring
+    error=$?
+    if [ ${error} -ne 0 ]; then
+      echo "Error (${error}): can not create ${WORKSPACE_PATH}/.keyring file"
+      break
+    fi
   fi
-  export BUILDER_WITH_COMMON_ARGS="${WORKSPACE_PATH}/${BUILDER_FILE} --keep-proxy --buckminster.root.prefix=${WORKSPACE_PATH} --log-level=DEBUG --workspace ${WORKSPACE_PATH}/${PRODUCT_FOLDER} --keyring ${WORKSPACE_PATH}/.keyring"
+  export BUILDER_WITH_COMMON_ARGS="${WORKSPACE_PATH}/${BUILDER_FILE} --keep-proxy --buckminster.root.prefix=${WORKSPACE_PATH} --log-level=DEBUG --workspace ${WORKSPACE_PATH}/${PRODUCT_WORKSPACE_FOLDER} --keyring ${WORKSPACE_PATH}/.keyring"
   export PATH=${WORKSPACE_PATH}/${BUCKMINSTER_FOLDER}:$PATH
   break
 done
--- org.dawnsci.base.site/dViewer_headless_materialize_master.sh
+++ org.dawnsci.base.site/dViewer_headless_materialize_master.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 
+PRODUCT_NAME_VERSION="dawn master"
+BUILD_LOG_FILE="build.log"
+
 source dViewer_buckminster_init.sh $@
 error=$?
 [ "${BASH_SOURCE[0]}" != "${0}" ] && THIS_SOURCED=1 || THIS_SOURCED=0
-if [ ${error} -ne 0 ]; then
-  echo "Error (${error}): initialisation failed"
-  [[ "${THIS_SOURCED}" -ne 0 ]] && return ${error} || exit ${error}
-fi
 
-if [ -z "${BUILDER_WITH_COMMON_ARGS}" ]; then
-  error=1
-  echo "Error (${error}): BUILDER_WITH_COMMON_ARGS is not specified"
-  [[ "${THIS_SOURCED}" -ne 0 ]] && return ${error} || exit ${error}
-fi
-
-PRODUCT_NAME_VERSION="dawn master"
-
-error=0
 while true; do
   cur_path=`pwd`
+  if [ ${error} -ne 0 ]; then
+    echo "Error (${error}): ${PRODUCT_ID} buckminster initialization failed"
+    break
+  fi
+  if [ -z "${BUILDER_WITH_COMMON_ARGS}" ]; then
+    error=1
+    echo "Error (${error}): BUILDER_WITH_COMMON_ARGS is not specified"
+    break
+  fi
   #python ${BUILDER_WITH_COMMON_ARGS} --delete -Ddownload.location.common=public materialize dawnbase dawn master
   echo "Materializing ${PRODUCT_NAME_VERSION} component and its dependencies."
-  python ${BUILDER_WITH_COMMON_ARGS} -Ddownload.location.common=public materialize ${PRODUCT_FOLDER} ${PRODUCT_NAME_VERSION}
+  python ${BUILDER_WITH_COMMON_ARGS} -Ddownload.location.common=public materialize ${PRODUCT_WORKSPACE_FOLDER} ${PRODUCT_NAME_VERSION}
   error=$?
   if [ ${error} -ne 0 ]; then
     echo "Error (${error}): can not materialize component and its dependencies"
     break
   fi
-  PRODUCT_SITE_FOLDER="${PRODUCT_FOLDER}_git/dawn-product.git/org.dawnsci.base.site"
+  PRODUCT_SITE_FOLDER="${PRODUCT_WORKSPACE_FOLDER}_git/dawn-product.git/org.dawnsci.base.site"
   if [ ! -d ${PRODUCT_SITE_FOLDER} ]; then
     error=1
     echo "Error (${error}): ${PRODUCT_SITE_FOLDER} does not exist"
@@ -49,7 +48,7 @@ while true; do
   python ${BUILDER_WITH_COMMON_ARGS} clean
   error=$?
   if [ ${error} -ne 0 ]; then
-    echo "Error (${error}): can not clean the workspace"
+    echo "Error (${error}): can not clean the workspace (by clean)"
     break
   fi
 #  echo "Building ${PRODUCT_NAME_VERSION} workspace."
@@ -63,7 +62,7 @@ while true; do
   python ${BUILDER_WITH_COMMON_ARGS} bmclean
   error=$?
   if [ ${error} -ne 0 ]; then
-    echo "Error (${error}): can not clean previous buckminster output"
+    echo "Error (${error}): can not clean previous buckminster output (by bmclean)"
     break
   fi
   #bmclean does not clean everything (why?), so better deleting the contents of output folder
@@ -82,9 +81,10 @@ while true; do
     break
   fi
   echo "Building ${PRODUCT_NAME_VERSION} workspace and Eclipse product, then zip the product."
-  python ${BUILDER_WITH_COMMON_ARGS} product.zip org.dawnsci.base.site linux32 linux64
+  python ${BUILDER_WITH_COMMON_ARGS} product.zip org.dawnsci.base.site linux32 linux64 >${BUILD_LOG_FILE} 2>&1
   error=$?
   if [ ${error} -ne 0 ]; then
+    grep Error ${BUILD_LOG_FILE}
     echo "Error (${error}): can not build workspace and Eclipse product, thus can not zip the product"
     break
   fi
