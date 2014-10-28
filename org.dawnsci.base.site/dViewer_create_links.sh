f0798f240e2ee49f8c9e562e03eb2cbe047e90a3
Gábor Náray <gnaray@embl.fr>
2014-10-28 13:25:59 +0100

    Simplified symlink creation.

--- org.dawnsci.base.site/dViewer_create_links.sh
+++ org.dawnsci.base.site/dViewer_create_links.sh
@@ -2,93 +2,35 @@
 #Input: must be run from the PRODUCT_ID/VERSION folder (see dViewer_pxsoft_installer.sh)
 #Output: symbolic links for bin folder and operating systems
 
+LINUX64_LINK="linux-x86_64"
+LINUX32_LINK="linux-i686"
+
 [ "${BASH_SOURCE[0]}" != "${0}" ] && THIS_SOURCED=1 || THIS_SOURCED=0
 
-CURRENT_PATH=`pwd`
+CURRENT_PATH="$(pwd)"
 
-#Why removing and creating symbolic links again? Because if it exists, then it is created in dViewer* folder (=nonsense).
 error=0
 while true; do
-  rm -f linux.i686
-  ln -s `ls -d dViewer*-linux32` linux.i686
-  error=$?
+  ln -fns `ls -d dViewer*-linux32` "${LINUX32_LINK}"
+  error=${?}
   if [ ${error} -ne 0 ]; then
     echo "Error (${error}): can not create symbolic link in ${PRODUCT_TARGET_PATH} folder"
     break
   fi
-  rm -f linux.x86_64
-  ln -s `ls -d dViewer*-linux64` linux.x86_64
-  error=$?
+  ln -fns `ls -d dViewer*-linux64` "${LINUX64_LINK}"
+  error=${?}
   if [ ${error} -ne 0 ]; then
     echo "Error (${error}): can not create symbolic link in ${PRODUCT_TARGET_PATH} folder"
     break
   fi
-  rm -f centos5-i686
-  ln -s linux.i686 centos5-i686
-  error=$?
+  ln -fns ../../generic/bin "${LINUX32_LINK}"/bin
+  error=${?}
   if [ ${error} -ne 0 ]; then
     echo "Error (${error}): can not create symbolic link in ${PRODUCT_TARGET_PATH} folder"
     break
   fi
-  rm -f centos5-x86_64
-  ln -s linux.x86_64 centos5-x86_64
-  error=$?
-  if [ ${error} -ne 0 ]; then
-    echo "Error (${error}): can not create symbolic link in ${PRODUCT_TARGET_PATH} folder"
-    break
-  fi
-  rm -f debian6-i686
-  ln -s linux.i686 debian6-i686
-  error=$?
-  if [ ${error} -ne 0 ]; then
-    echo "Error (${error}): can not create symbolic link in ${PRODUCT_TARGET_PATH} folder"
-    break
-  fi
-  rm -f debian6-x86_64
-  ln -s linux.x86_64 debian6-x86_64
-  error=$?
-  if [ ${error} -ne 0 ]; then
-    echo "Error (${error}): can not create symbolic link in ${PRODUCT_TARGET_PATH} folder"
-    break
-  fi
-  rm -f gentoo-i686
-  ln -s linux.i686 gentoo-i686
-  error=$?
-  if [ ${error} -ne 0 ]; then
-    echo "Error (${error}): can not create symbolic link in ${PRODUCT_TARGET_PATH} folder"
-    break
-  fi
-  rm -f gentoo-x86_64
-  ln -s linux.x86_64 gentoo-x86_64
-  error=$?
-  if [ ${error} -ne 0 ]; then
-    echo "Error (${error}): can not create symbolic link in ${PRODUCT_TARGET_PATH} folder"
-    break
-  fi
-  rm -f redhate5-i686
-  ln -s linux.i686 redhate5-i686
-  error=$?
-  if [ ${error} -ne 0 ]; then
-    echo "Error (${error}): can not create symbolic link in ${PRODUCT_TARGET_PATH} folder"
-    break
-  fi
-  rm -f redhate5-x86_64
-  ln -s linux.x86_64 redhate5-x86_64
-  error=$?
-  if [ ${error} -ne 0 ]; then
-    echo "Error (${error}): can not create symbolic link in ${PRODUCT_TARGET_PATH} folder"
-    break
-  fi
-  rm -f linux.i686/bin
-  ln -s ../../generic/bin linux.i686/bin
-  error=$?
-  if [ ${error} -ne 0 ]; then
-    echo "Error (${error}): can not create symbolic link in ${PRODUCT_TARGET_PATH} folder"
-    break
-  fi
-  rm -f linux.x86_64/bin
-  ln -s ../../generic/bin linux.x86_64/bin
-  error=$?
+  ln -fns ../../generic/bin "${LINUX64_LINK}"/bin
+  error=${?}
   if [ ${error} -ne 0 ]; then
     echo "Error (${error}): can not create symbolic link in ${PRODUCT_TARGET_PATH} folder"
     break
@@ -96,7 +38,7 @@ while true; do
   echo "Symbolic links are created successfully."
   break
 done
-cd ${CURRENT_PATH}
+cd "${CURRENT_PATH}"
 if [ ${error} -ne 0 ]; then
   echo "Error (${error}): creating of symbolic links failed"
 fi
